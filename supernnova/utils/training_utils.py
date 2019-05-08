fixed normalization bug in validation w external model

supernnova/utils/training_utils.py
@@ -42,12 +42,10 @@ def normalize_arr(arr, settings):
     arr_std = settings.arr_norm[:, 2]
 
     arr_to_norm = arr[:, settings.idx_features_to_normalize]
+    # clipping
+    arr_to_norm = np.clip(arr_to_norm, arr_min, np.inf)
 
-    subtracted_min = arr_to_norm - arr_min
-    # setting to min values below normalization
-    # necessary if using an already trained model
-    subtracted_min[subtracted_min<0] = 0
-    arr_normed = np.log(subtracted_min + 1e-5)
+    arr_normed = np.log(arr_to_norm - arr_min + 1e-5)
     arr_normed = (arr_normed - arr_mean) / arr_std
 
     arr[:, settings.idx_features_to_normalize] = arr_normed
@@ -120,11 +118,18 @@ def fill_data_list(
         # Keep an unnormalized copy of the data (for test and display)
         X_ori = X_all.copy()[:, settings.idx_features]
 
+        # check if normalization converges
+        # using clipping in case of min<model_min
+        X_tmp = unnormalize_arr(normalize_arr(X_all.copy(),settings), settings)
+        X_clip = X_all.copy()
+        X_clip = np.clip(X_clip[:,settings.idx_features_to_normalize], settings.arr_norm[:, 0], np.inf)
+        X_all[:,settings.idx_features_to_normalize] = X_clip
+        assert np.all(np.all(np.isclose(np.ravel(X_all), np.ravel(X_tmp), atol=1e-2)))
         # Normalize features that need to be normalized
         X_normed = X_all.copy()
-        X_normed = normalize_arr(X_normed, settings)
+        X_normed_tmp = normalize_arr(X_normed, settings)
         # Select features as specified by the settings
-        X_normed = X_normed[:, settings.idx_features]
+        X_normed = X_normed_tmp[:, settings.idx_features]
 
         if test is True:
             list_data.append((X_normed, target, lc, X_all, X_ori))

supernnova/validation/validate_rnn.py
@@ -1,6 +1,5 @@
 import os
 import math
-import json
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -85,7 +84,7 @@ def get_batch_predictions_MFE(rnn, X, target):
     return arr_preds, arr_target
 
 
-def get_predictions(settings, model_file = None):
+def get_predictions(settings, model_file=None):
     """Obtain predictions for a given RNN model specified by the
     ``settings`` argument or alternatively, by a model_file
 
@@ -100,33 +99,6 @@ def get_predictions(settings, model_file = None):
         settings (ExperimentSettings): custom class to hold hyperparameters
         model_file (str): Path to saved model weights. Default: ``None``
     """
-    # load normalization from model
-    # (which is not in model settings but dumped in json)
-    if model_file:
-        fname = f"{Path(model_file).parent}/data_norm.json"
-        with open(fname, "r") as f:
-            dic_norm = json.load(f)
-
-        list_norm = []
-        for f in settings.training_features_to_normalize:
-            if settings.norm == "perfilter":
-                for f in self.training_features_to_normalize:
-                    minv = dic_norm[f]['min']
-                    meanv = dic_norm[f]['mean']
-                    stdv = dic_norm[f]['std']
-                    list_norm.append([minv, meanv, stdv])
-            else:
-                if "FLUX" in f:
-                    minv = dic_norm['FLUXCAL_g']['min']
-                    meanv = dic_norm['FLUXCAL_g']['mean']
-                    stdv = dic_norm['FLUXCAL_g']['std']
-                    list_norm.append([minv, meanv, stdv])
-                else:
-                    minv = dic_norm['FLUXCALERR_g']['min']
-                    meanv = dic_norm['FLUXCALERR_g']['mean']
-                    stdv = dic_norm['FLUXCALERR_g']['std']
-                    list_norm.append([minv, meanv, stdv])
-        settings.arr_norm = np.array(list_norm)
 
     settings.random_length = False
     settings.random_redshift = False

supernnova/visualization/early_prediction.py
@@ -200,7 +200,6 @@ def make_early_prediction(settings, nb_lcs=1, do_gifs=False):
     # Load RNN model
     dict_rnn = {}
     if settings.model_files is None:
-        import ipdb; ipdb.set_trace()
         settings.model_files = [f"{settings.rnn_dir}/{settings.pytorch_model_name}.pt"]
     else:
         # check if the model files are there
@@ -279,19 +278,12 @@ def make_early_prediction(settings, nb_lcs=1, do_gifs=False):
             X_unnormed = tu.unnormalize_arr(X_normed, settings)
             # Check we do recover X_ori when OOD is None
             if OOD is None:
-                try:
-                    assert np.all(
-                    np.isclose(np.ravel(X_ori), np.ravel(X_unnormed), atol=1e-2)
-                )
-                except Exception:
-                    # if this is due to the new min for the norm, continues
-                    idxs = np.where(~np.isclose(np.ravel(X_ori), np.ravel(X_unnormed), atol=1e-2))
-                    checking = []
-                    for idx in idxs:
-                        if np.ravel(X_ori)[idx]<np.ravel(settings.arr_norm).min():
-                            continue
-                        else:
-                            raise Exception
+                #check if normalization converges
+                # using clipping in case of min<model_min
+                X_clip = X_ori.copy()
+                X_clip = np.clip(X_clip[:,settings.idx_features_to_normalize], settings.arr_norm[:, 0], np.inf)
+                X_ori[:,settings.idx_features_to_normalize] = X_clip
+                assert np.all(np.all(np.isclose(np.ravel(X_ori), np.ravel(X_unnormed), atol=1e-2)))
 
             # TODO: IMPROVE
             df_temp = pd.DataFrame(data=X_unnormed, columns=features)