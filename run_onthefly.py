66b567e4d79eee783cb21602f6f04db184e54037
anaismoller <anaismoller@gmail.com>
2020-04-21 18:13:41 +0200

    supports csv/manual input + output csv

--- run_onthefly.py
+++ run_onthefly.py
@@ -7,12 +7,18 @@ from supernnova.validation.validate_onthefly import classify_lcs
 def manual_lc():
     """Manually provide data
     """
+    # this is the format you can use to provide light-curves
     df = pd.DataFrame()
+    # supernova IDs
+    df["SNID"] = ["1", "1", "2", "2"]
+    # time in MJD
     df["MJD"] = [57433.4816, 57436.4815, 33444, 33454]
+    # FLux and errors
     df["FLUXCAL"] = [2.0, 3, 200, 300]
     df["FLUXCALERR"] = [0.1, 0.2, 0.1, 0.2]
+    # bandpasses
     df["FLT"] = ["g", "r", "g", "r"]
-    df["SNID"] = ["1", "1", "2", "2"]
+    # redshift is not required if classifying without it
     df["HOSTGAL_SPECZ"] = [0.12, 0.12, 0.5, 0.5]
     df["HOSTGAL_PHOTZ"] = [0.1, 0.1, 0.5, 0.5]
 
@@ -27,6 +33,31 @@ def load_lc_csv(filename):
     df["SNID"] = df["SNID"].astype(int).astype(str)
 
     return df
+
+
+def reformat_to_df(pred_probs, ids=None):
+    # TO DO: suppport nb_inference != 1
+    num_inference_samples = 1
+
+    d_series = {}
+    for i in range(pred_probs[0].shape[1]):
+        d_series["SNID"] = []
+        d_series[f"prob_class{i}"] = []
+    for idx, value in enumerate(pred_probs):
+        d_series["SNID"] += ids[idx] if len(ids) > 0 else idx
+        value = value.reshape((num_inference_samples, -1))
+        value_dim = value.shape[1]
+        for i in range(value_dim):
+            d_series[f"prob_class{i}"].append(value[:, i][0])
+    preds_df = pd.DataFrame.from_dict(d_series)
+
+    # get predicted class
+    try:
+        preds_df["pred_class"] = np.argmax(pred_probs, axis=0)[0]
+    except Exception:
+        preds_df["pred_class"] = np.argmax(pred_probs[0], axis=0)[0]
+
+    return preds_df
 
 
 if __name__ == "__main__":
@@ -58,8 +89,11 @@ if __name__ == "__main__":
     df = manual_lc()
 
     # Obtain predictions for full light-curve
-    # Output format: batch, nb_inference_samples, nb_classes
-    preds = classify_lcs(df, args.model_file, args.device)
+    # Format: batch, nb_inference_samples, nb_classes
+    pred_probs = classify_lcs(df, args.model_file, args.device)
+
+    # reformat to df
+    preds_df = reformat_to_df(pred_probs, ids=df.SNID.unique())
 
     import ipdb
 
