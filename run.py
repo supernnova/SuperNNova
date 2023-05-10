e7bbed19bc3791fb4c4a681be19708a129314b18
anaismoller <anaismoller@gmail.com>
2023-05-11 09:51:15 +1000

    message

--- run.py
+++ run.py
@@ -136,9 +136,7 @@ if __name__ == "__main__":
             if settings.debug:
                 visualize.visualize(settings)
             else:
-                logging_utils.print_red(
-                    "Use --debug in database creation for explore_lightcurves"
-                )
+                logging_utils.print_red("Use --debug --data for explore_lightcurves")
 
         if settings.plot_lcs:
             if settings.model_files:
