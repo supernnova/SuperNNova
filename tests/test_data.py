f227ed94caca55b79084935c2a3f588c1705a5fe
JHu <jitinghu@swin.edu.au>
2023-12-04 20:51:56 +1100

    create a seperate script for testing dataset making

---
+++ tests/test_data.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+from .test_utils import call_cmd
+import pytest
+
+dir_data_path = os.path.dirname(os.path.realpath(__file__)) + "/dump_data/"
+
+
+@pytest.mark.parametrize(
+    "option",
+    [
+        "",
+        "--list_filters g r",
+        "--testing_ids tests/raw_csv/DES_HEAD.csv",
+        '--sntypes \'{"101":"Ia"}\' ',
+    ],
+)
+def test_dataset_making(option):
+    cmd = (
+        "python run.py --data --dump_dir tests/dump_data --raw_dir tests/raw " + option
+    )
+    call_cmd(cmd)
+
+    # check whether database has been generated
+    assert os.path.exists(dir_data_path + "processed/database.h5") is True
+    assert os.path.exists(dir_data_path + "processed/SNID.pickle") is True
+    assert os.path.exists(dir_data_path + "processed/hostspe_SNID.pickle") is True
+    shutil.rmtree(dir_data_path)
