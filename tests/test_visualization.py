e3977e073bde7259589b412d7542918b2a246242
JHu <jitinghu@swin.edu.au>
2024-05-27 12:18:25 +1000

    update visualization tests

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -11,23 +11,14 @@ class TestVisualization:
     @pytest.fixture(autouse=True, scope="class")
     def setup_class(self):
         shutil.rmtree(dir_data_path, ignore_errors=True)
-        cmd1 = "snn --data --dump_dir tests/dump_visual --raw_dir tests/raw --debug"
+        cmd1 = "snn make_data --dump_dir tests/dump_visual --raw_dir tests/raw"
         call_cmd(cmd1)
-        cmd2 = "snn --train_rnn --dump_dir tests/dump_visual --nb_epoch=10"
+        cmd2 = "snn train_rnn --dump_dir tests/dump_visual --nb_epoch=10"
         call_cmd(cmd2)
-        cmd3 = "snn --validate_rnn --dump_dir tests/dump_visual"
+        cmd3 = "snn validate_rnn --dump_dir tests/dump_visual"
         call_cmd(cmd3)
         yield
         shutil.rmtree(dir_data_path, ignore_errors=True)
-
-    def test_explore_lightcurve(self):
-        cmd = "snn --dump_dir tests/dump_visual --explore_lightcurves"
-        call_cmd(cmd)
-        assert os.path.exists(dir_data_path + "explore/sample_lightcurves.png") is True
-        assert (
-            os.path.exists(dir_data_path + "explore/sample_lightcurves_from_hdf5.png")
-            is True
-        )
 
     def test_plot_lcs(self):
         # remove lightcurves generated during --train_rnn
@@ -38,7 +29,10 @@ class TestVisualization:
         assert len(model_files) == 1
 
         model_file = model_files[0]
-        cmd = "snn --dump_dir tests/dump_visual --plot_lcs --model_files  " + model_file
+        cmd = (
+            "snn show --dump_dir tests/dump_visual --plot_lcs --model_files  "
+            + model_file
+        )
         call_cmd(cmd)
 
         lc_files = glob.glob(dir_data_path + "lightcurves/*/early_prediction/*.png")
@@ -55,7 +49,7 @@ class TestVisualization:
         model_file = model_files[0]
         # the csv file not exist; it will trigger to plot 2 random lightcurves
         cmd = (
-            "snn --dump_dir tests/dump_visual --plot_lcs --model_files  "
+            "snn show --dump_dir tests/dump_visual --plot_lcs --model_files  "
             + model_file
             + " --plot_file trigger_random.csv"
         )
