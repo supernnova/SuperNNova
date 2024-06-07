3751909cef635f1e17eda8604751241ad603c61c
JHu <jitinghu@swin.edu.au>
2024-06-08 09:46:03 +1000

    update modular run example

--- sandbox/example_run_modular_snn.py
+++ sandbox/example_run_modular_snn.py
@@ -6,32 +6,36 @@ from supernnova.validation import validate_rnn
 """Example for running SuperNNova as a module
 
 if installed by "pip install supernnova"
-you can run this code in the parent folder (where run.py is)
+you can run this code in the parent folder (where tests/ is)
 """
 
-# get config args
-args = conf.get_args()
+if __name__ == "__main__":
+    # get config args
+    command_arg = "make_data"
+    args = conf.get_args(command_arg)
 
-# create database
-args.data = True  # conf: making new dataset
-args.dump_dir = "tests/dump"  # conf: where the dataset will be saved
-args.raw_dir = "tests/raw"  # conf: where raw photometry files are saved
-args.fits_dir = "tests/fits"  # conf: where salt2fits are saved
-settings = conf.get_settings(args)  # conf: set settings
-make_dataset.make_dataset(settings)  # make dataset
+    # create database
+    args.dump_dir = "tests/dump"  # conf: where the dataset will be saved
+    args.raw_dir = "tests/raw"  # conf: where raw photometry files are saved
+    args.fits_dir = "tests/fits"  # conf: where salt2fits are saved
+    settings = conf.get_settings(command_arg, args)  # conf: set settings
+    make_dataset.make_dataset(settings)  # make dataset
 
-# train model
-args.data = False  # conf: no database creation
-args.train_rnn = True  # conf: train rnn
-args.dump_dir = "tests/dump"  # conf: where the dataset is saved
-args.nb_epoch = 2  # conf: training epochs
-settings = conf.get_settings(args)  # conf: set settings
-train_rnn.train(settings)  # train rnn
+    # get config args
+    command_arg = "train_rnn"
+    args = conf.get_args(command_arg)
 
-# validate (test set classificatio)
-args.data = False  # conf: no database creation
-args.train_rnn = False  # conf: no train rnn
-args.validate_rnn = False  # conf: validate rnn
-args.dump_dir = "tests/dump"  # conf: where the dataset is saved
-settings = conf.get_settings(args)  # conf: set settings
-validate_rnn.get_predictions(settings)  # classify test set
+    # train rnn
+    args.dump_dir = "tests/dump"  # conf: where the dataset is saved
+    args.nb_epoch = 2  # conf: training epochs
+    settings = conf.get_settings(command_arg, args)  # conf: set settings
+    train_rnn.train(settings)  # train rnn
+
+    # get config args
+    command_arg = "validate_rnn"
+    args = conf.get_args(command_arg)
+
+    # validate rnn
+    args.dump_dir = "tests/dump"  # conf: where the dataset is saved
+    settings = conf.get_settings(command_arg, args)  # conf: set settings
+    validate_rnn.get_predictions(settings)  # classify test set
