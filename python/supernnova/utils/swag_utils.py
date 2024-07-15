c2a3923edb1cc1b712919de29e6562f5b5ca87f4
JHu <jitinghu@swin.edu.au>
2024-07-15 12:53:06 +1000

    update swagmodel doc string

--- python/supernnova/utils/swag_utils.py
+++ python/supernnova/utils/swag_utils.py
@@ -47,72 +47,31 @@ def unflatten_like(vector, likeTensorList):
 
 
 class SwagModel(Module):
-    r"""Implements averaged model for Stochastic Weight Averaging (SWA).
+    r"""Implements averaged model for Stochastic Weight Averaging Gaussian (SWAG).
 
-    Stochastic Weight Averaging was proposed in `Averaging Weights Leads to
-    Wider Optima and Better Generalization`_ by Pavel Izmailov, Dmitrii
-    Podoprikhin, Timur Garipov, Dmitry Vetrov and Andrew Gordon Wilson
-    (UAI 2018).
+    SWAG was proposed in `A Simple Baseline for Bayesian Uncertainty
+    in Deep Learning` by Wesley J. Maddox, Timur Garipov, Pavel Izmailov,
+    Dmitry Vetrov and Andrew Gordon Wilson in 2019.
 
-    AveragedModel class creates a copy of the provided module :attr:`model`
+    SwagModel is a modified version of `torch.optim.swa_utils.AveragedModel` (`pytorch` v2.0.1), which
+    implements Stochastic Weight Averaging (SWA). This modification is inspired by
+    the original implementation of SWAG available at: https://github.com/wjmaddox/swa_gaussian.
+
+    SwagModel class creates a copy of the provided module :attr:`model`
     on the device :attr:`device` and allows to compute running averages of the
     parameters of the :attr:`model`.
 
     Args:
-        model (torch.nn.Module): model to use with SWA
+        model (torch.nn.Module): model to use with SWAG
         device (torch.device, optional): if provided, the averaged model will be
-            stored on the :attr:`device`
-        avg_fn (function, optional): the averaging function used to update
+            stored on the :attr:`device` (default: ``None``)
+        avg_fn (function): the averaging function used to update
             parameters; the function must take in the current value of the
-            :class:`AveragedModel` parameter, the current value of :attr:`model`
+            :class:`SwagModel` parameter, the current value of :attr:`model`
             parameter and the number of models already averaged; if None,
-            equally weighted average is used (default: None)
+            equally weighted average is used (default: ``swa_update``)
         use_buffers (bool): if ``True``, it will compute running averages for
             both the parameters and the buffers of the model. (default: ``False``)
-
-    Example:
-        >>> # xdoctest: +SKIP("undefined variables")
-        >>> loader, optimizer, model, loss_fn = ...
-        >>> swa_model = torch.optim.swa_utils.AveragedModel(model)
-        >>> scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
-        >>>                                     T_max=300)
-        >>> swa_start = 160
-        >>> swa_scheduler = SWALR(optimizer, swa_lr=0.05)
-        >>> for i in range(300):
-        >>>      for input, target in loader:
-        >>>          optimizer.zero_grad()
-        >>>          loss_fn(model(input), target).backward()
-        >>>          optimizer.step()
-        >>>      if i > swa_start:
-        >>>          swa_model.update_parameters(model)
-        >>>          swa_scheduler.step()
-        >>>      else:
-        >>>          scheduler.step()
-        >>>
-        >>> # Update bn statistics for the swa_model at the end
-        >>> torch.optim.swa_utils.update_bn(loader, swa_model)
-
-    You can also use custom averaging functions with `avg_fn` parameter.
-    If no averaging function is provided, the default is to compute
-    equally-weighted average of the weights.
-
-    Example:
-        >>> # xdoctest: +SKIP("undefined variables")
-        >>> # Compute exponential moving averages of the weights and buffers
-        >>> ema_avg = lambda averaged_model_parameter, model_parameter, num_averaged: (
-        ...                 0.1 * averaged_model_parameter + 0.9 * model_parameter)
-        >>> swa_model = torch.optim.swa_utils.AveragedModel(model, avg_fn=ema_avg, use_buffers=True)
-
-    .. note::
-        When using SWA with models containing Batch Normalization you may
-        need to update the activation statistics for Batch Normalization.
-        This can be done either by using the :meth:`torch.optim.swa_utils.update_bn`
-        or by setting :attr:`use_buffers` to `True`. The first approach updates the
-        statistics in a post-training step by passing data through the model. The
-        second does it during the parameter update phase by averaging all buffers.
-        Empirical evidence has shown that updating the statistics in normalization
-        layers increases accuracy, but you may wish to empirically test which
-        approach yields the best results in your problem.
 
     .. note::
         :attr:`avg_fn` is not saved in the :meth:`state_dict` of the model.
@@ -120,20 +79,9 @@ class SwagModel(Module):
     .. note::
         When :meth:`update_parameters` is called for the first time (i.e.
         :attr:`n_averaged` is `0`) the parameters of `model` are copied
-        to the parameters of :class:`AveragedModel`. For every subsequent
+        to the parameters of :class:`SwagModel`. For every subsequent
         call of :meth:`update_parameters` the function `avg_fn` is used
         to update the parameters.
-
-    .. _Averaging Weights Leads to Wider Optima and Better Generalization:
-        https://arxiv.org/abs/1803.05407
-    .. _There Are Many Consistent Explanations of Unlabeled Data: Why You Should
-        Average:
-        https://arxiv.org/abs/1806.05594
-    .. _SWALP: Stochastic Weight Averaging in Low-Precision Training:
-        https://arxiv.org/abs/1904.11943
-    .. _Stochastic Weight Averaging in Parallel: Large-Batch Training That
-        Generalizes Well:
-        https://arxiv.org/abs/2001.02312
     """
 
     def __init__(
