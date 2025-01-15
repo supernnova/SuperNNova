94849192ce9e9464853657f68b0e02d8303ea458
Anais Möller <anaismoller@gmail.com>
2025-01-15 18:21:50 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     name="supernnova",
     version=supernnova.__version__,
     author="Anais Moller and Thibault de Boissiere",
-    author_email="anais.moller@clermont.in2p3.fr",
+    author_email="amoller@swin.edu.au",
     description="framework for Bayesian, Neural Network based supernova light-curve classification",
     url="https://github.com/supernnova/SuperNNova",
     packages=setuptools.find_packages(),
